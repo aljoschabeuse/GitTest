cbad9398e92bcc427a9c84274c7ea12e8bf3e5ba
Aljoscha Beuse <aljoscha.beuse@uni-muenster.de>
2021-09-29 14:35:29 +0200

    Today I'm feeling good.

---
+++ GitTest-master/src/application/Main.java
@@ -0,0 +1,32 @@
+package application;
+	
+import java.net.URL;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			BorderPane root = new BorderPane();
+			Scene scene = new Scene(root,400,400);
+			URL urlStyles = getClass().getResource("application.css");
+			scene.getStylesheets().add(urlStyles.toExternalForm());
+			Label hello = new Label("Hello World!\nI'm feeling good today.");
+			root.setCenter(hello);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
